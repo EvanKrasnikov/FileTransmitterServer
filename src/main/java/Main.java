3411eac421d07e77ec2f7f1e1bc4b66a57c4a938
Ivan Krasnikov <geographer29@yandex.ru>
2018-01-02 18:44:57 +0300

    Main and Server classes refactored using nio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,8 @@
-import auth.ClientHandler;
-
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
-    private static int port = 8189;
-
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("Сервер запущен, ожидаем подключения");
-
-            while(true){
-                socket = serverSocket.accept();
-                new ClientHandler(socket);
-                System.out.println("Клиент подключился");
-            }
-
-        } catch (IOException e) {
-            System.err.println("Ошибка инициализации сервера");
-        } finally {
-            if (serverSocket != null){
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    System.err.println("Не удалось закрыть порт");
-                }
-            }
-        }
+    public static void main(String[] args) throws IOException{
+        Server server = new Server();
     }
 }
 
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,67 +1,67 @@
-
 import auth.ClientHandler;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Vector;
 
 public class Server {
     private final int PORT = 8189;
+    private final String HOST_ADRESS = "localhost";
     private Vector<ClientHandler> clients;
     //private AuthService authService;
     private static final int BUFFER_SIZE = 1024;
     private static Selector selector = null;
 
 
-    public Server(){
-        ServerSocket server = null;
-        Socket socket = null;
-        authService = null;
+    public Server() throws IOException{
+        //authService = null;
         clients = new Vector<>();
+        selector = Selector.open();
 
-        try{
-            ServerSocketChannel socketChannel = ServerSocketChannel.open();
-            ServerSocket serverSocket = socketChannel.socket();
+        ServerSocketChannel socketChannel = ServerSocketChannel.open();
+        InetSocketAddress socketAddress = new InetSocketAddress(HOST_ADRESS, PORT);
 
-            InetAddress hostAdress = InetAddress.getLocalHost();
-            InetSocketAddress socketAddress = new InetSocketAddress(hostAdress, PORT);
-            serverSocket.bind(socketAddress);
+        socketChannel.bind(socketAddress);
+        socketChannel.configureBlocking(false);
 
-            socketChannel.configureBlocking(false);
-            int ops = socketChannel.validOps();
-            socketChannel.register(selector, ops, null);
+        int ops = socketChannel.validOps();
+        socketChannel.register(selector, ops, null);
 
-            while (true) {
-                selector.select();
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                Iterator<SelectionKey> i = selectedKeys.iterator();
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> i = selectedKeys.iterator();
 
-                while (i.hasNext()) {
-                    SelectionKey key = i.next();
+            while (i.hasNext()) {
+                SelectionKey key = i.next();
 
-                    if (key.isAcceptable()) {
-                        processAcceptEvent(socketChannel, key);
-                    } else if (key.isReadable()) {
-                        processReadEvent(key);
-                    }
-                    i.remove();
+                if (key.isAcceptable()) {
+                    processAcceptEvent(socketChannel, key);
+                } else if (key.isReadable()) {
+                    processReadEvent(key);
                 }
+
+                i.remove();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+    }
 
-            System.out.println("Сервер запущен, ждем клиентов");
+    private static void processAcceptEvent(ServerSocketChannel socketChannel, SelectionKey key) throws IOException{
+            SocketChannel client = socketChannel.accept();
+            client.configureBlocking(false);
+            client.register(selector,SelectionKey.OP_WRITE);
+    }
 
+    private static void processReadEvent(SelectionKey key) throws IOException{
+        SocketChannel client = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        client.read(buffer);
+        String result = new String(buffer.array()).trim();
 
-        }
+        ClientHandler clientHandler = new ClientHandler();
     }
 }
