df7ef741e8ed9f17fc44c2e103ffdce30e1b630f
Ivan Krasnikov <geographer29@yandex.ru>
2018-01-06 21:21:17 +0300

    Added BaseHandler class

---
+++ src/main/java/basehandler/BaseHandler.java
@@ -0,0 +1,55 @@
+package basehandler;
+
+import auth.Authorization;
+import storage.Storage;
+
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.util.ArrayDeque;
+
+public class BaseHandler {
+    private ServerSocketChannel socket;
+    private SelectionKey key;
+    private String name;
+    private String login;
+    private String pass;
+    private Authorization authorization;
+    private Storage storage;
+
+    public BaseHandler(ServerSocketChannel socket, SelectionKey key) {
+        this.socket = socket;
+        this.key = key;
+        name = "";
+    }
+
+    public void parseMassage(ArrayDeque<String> queue){
+        switch (queue.pop()){
+            case "/login" :{
+                login = queue.pop();
+                pass = queue.pop();
+                authorization = new Authorization();
+                authorization.loginValidation();
+
+                if (authorization.isAuthorizationOk){
+                    name = login;
+                    storage = new Storage();
+                }
+            }
+
+            case "/register" : {
+                login = queue.pop();
+                pass = queue.pop();
+                authorization = new Authorization();
+                authorization.registerUser();
+            }
+
+            case "/add" :{
+                storage.receiveFile(queue);
+            }
+
+            case "/remove" : {
+                storage.removeFile(queue);
+            }
+        }
+    }
+}
