b3418727e5a3ded7caa1375f26e5e8ce311e3df2
i.krasnikov <geographer29@yandex.ru>
2018-01-24 15:41:23 +0300

    DataBase issues are separated from Authorization

--- src/main/java/auth/Authorization.java
+++ src/main/java/auth/Authorization.java
@@ -2,91 +2,35 @@ package auth;
 
 import speaker.Message;
 
-import java.sql.*;
-
 public class Authorization{
-    private Connection connection;
-    private PreparedStatement statement;
-    private final String DATABASE_PATH = "C:/TMP/DB/database.db";
-    private final String PORT = "5050";
     private String login;
     private String pass;
     private Message msg = new Message();
     public boolean isAuthorizationOk = false;
+    private DataBase dataBase;
 
-    private void connectToDB() throws ClassNotFoundException, SQLException{ // Подключение к базе данных
-        Class.forName("org.sqlite.JDBC");
-        System.out.println("SQL driver loaded");
-
-        connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH + ":" + PORT);
-        System.out.println("Connection to the database established");
-    }
-
-    private void disconnectWithDB() throws SQLException{ // Отключение от базы данных
-        statement.close();
-        connection.close();
-    }
-
-    private boolean isExist() throws SQLException{   // Проверка существования пользователя
-        statement = connection.prepareStatement("SELECT count(*) FROM users where login = ?");
-        statement.setString(1,login);
-        ResultSet result = statement.executeQuery();
-
-        if (result.getString(1).equals("0")) {
-            msg.sendMessage("/nosuchuser");
-        } else return true;
-
+    private boolean isExist(){   // Проверка существования пользователя
+        if (dataBase.checkLogin(login).equals("0")) msg.sendMessage("/nosuchuser");
+            else return true;
         return false;
     }
 
     public void registerUser(){ // Регистрация пользователя
-        try {
-            connectToDB();
-
-            if (!isExist()) {
-                statement = connection.prepareStatement("INSERT INTO users (login, pass) VALUES (?,?)");
-                statement.setString(1,login);
-                statement.setString(2,pass);
-                msg.sendMessage("/registrationok");
-                isAuthorizationOk = true;
-            } else {
-                msg.sendMessage("/loginisoccupied");
-            }
-
-
-        } catch (ClassNotFoundException | SQLException  e){
-            e.printStackTrace();
-        } finally {
-            try {
-                disconnectWithDB();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        if (!isExist()) {
+            dataBase.registerUser(login,pass);
+            msg.sendMessage("/registrationok");
+            isAuthorizationOk = true;
+        } else {
+            msg.sendMessage("/loginisoccupied");
         }
     }
 
     public void loginValidation(){ // Checking if login is valid
-        try {
-            connectToDB();
-
-            statement = connection.prepareStatement("SELECT login, pass FROM users where login = ?");
-            statement.setString(1,login);
-            ResultSet result = statement.executeQuery();
-
-            if ((!result.getString(1).equals(login)) || (!result.getString(2).equals(pass))){  // проверка правильности логина и пароля
-                msg.sendMessage("/incorrectpass");
-            } else {
-                msg.sendMessage("/incorrectpass");
-                isAuthorizationOk = true;
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                disconnectWithDB();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        if (dataBase.getPass(login).equals(pass)){  // проверка правильности логина и пароля
+            msg.sendMessage("/correctpass");
+            isAuthorizationOk = true;
+        } else {
+            msg.sendMessage("/incorrectpass");
         }
     }
 }
