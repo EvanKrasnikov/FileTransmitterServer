6df5547147d0fac5089e7f617e3fc575c6193518
Ivan Krasnikov <geographer29@yandex.ru>
2018-02-22 18:01:06 +0300

    Methods of class are static now

--- src/main/java/auth/Authorization.java
+++ src/main/java/auth/Authorization.java
@@ -1,38 +1,34 @@
 package auth;
 
-import speaker.Message;
-import speaker.Messages;
-
+import server.Session;
+import utils.Messages;
 
 public class Authorization implements Messages {
-    private String login;
-    private String pass;
-    private Message msg = new Message();
-    public boolean isAuthorizationOk = false;
-    private DataBase dataBase;
 
-    private boolean isExist(){   // Проверка существования пользователя
-        if (dataBase.checkLogin(login).equals("0")) msg.sendMessage(NO_SUCH_USER);
+    private static boolean isExist(String login){   // Проверка существования пользователя
+        if (DataBase.checkLogin(login).equals("0")) Session.sendMessage(NO_SUCH_USER);
             else return true;
         return false;
     }
 
-    public void registerUser(){ // Регистрация пользователя
-        if (!isExist()) {
-            dataBase.registerUser(login,pass);
-            msg.sendMessage(REGISTRATION_COMPLETED);
-            isAuthorizationOk = true;
+    public static boolean registerUser(String login, String pass){ // Регистрация пользователя
+        if (!isExist(login)) {
+            DataBase.registerUser(login,pass);
+            Session.sendMessage(REGISTRATION_COMPLETED);
+            return true;
         } else {
-            msg.sendMessage(LOGIN_IS_OCCUPIED);
+            Session.sendMessage(LOGIN_IS_OCCUPIED);
+            return false;
         }
     }
 
-    public void loginValidation(){ // Checking if login is valid
-        if (dataBase.getPass(login).equals(pass)){  // проверка правильности логина и пароля
-            msg.sendMessage(CORRECT_PASS);
-            isAuthorizationOk = true;
+    public static boolean validateLogin(String login, String pass){ // Checking if login is valid
+        if (DataBase.getPass(login).equals(pass)){  // проверка правильности логина и пароля
+            Session.sendMessage(CORRECT_PASS);
+            return true;
         } else {
-            msg.sendMessage(INCORRECT_PASS);
+            Session.sendMessage(INCORRECT_PASS);
+            return false;
         }
     }
 }
