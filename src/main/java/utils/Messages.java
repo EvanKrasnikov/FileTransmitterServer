2cdeb95e9d8c0625b9949d01126c4e6a7c97232f
Ivan Krasnikov <geographer29@yandex.ru>
2018-02-22 17:55:16 +0300

    Few organization changes

--- src/main/java/utils/Messages.java
+++ src/main/java/utils/Messages.java
@@ -5,15 +5,19 @@ public interface Messages {
     String CORRECT_PASS = "CORRECT_PASS";
     String INCORRECT_PASS = "INCORRECT_PASS";
     String LOGIN_IS_OCCUPIED = "LOGIN_IS_OCCUPIED";
-    String TRANSITION_COMPLETE = "TRANSITION_COMPLETE";
+    String FAILED_TO_CONNECT = "FAILED_TO_CONNECT";
+    String LOGIN = "LOGIN";
+
     String REGISTRATION_COMPLETED = "REGISTRATION_COMPLETED";
     String REGISTER = "REGISTER";
+
     String ADD = "ADD";
     String REMOVE = "REMOVE";
-    String LOGIN = "LOGIN";
     String GET_LIST = "GET_LIST";
     String GET_FILES = "GET_FILES";
+    String TRANSITION_COMPLETE = "TRANSITION_COMPLETE";
 }
+
 
 
 
